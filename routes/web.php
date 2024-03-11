1886ac88837c2664c629ee9d210bc24753a0623e
Kaiba777 <sawadogomartial222@gmail.com>
2024-03-11 22:43:37 +0000

    Implementation de option inscription

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\private\abonne\AbonneTableaudebordController;
 use App\Http\Controllers\private\admin\AdminTableaudebordController;
 use App\Http\Controllers\private\promoteur\PromoteurTableaudebordController;
 use App\Http\Controllers\public\AcceuilController;
+use App\Http\Controllers\public\AuthController;
 use App\Http\Controllers\public\EvenementController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,11 +18,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// Public routes
 Route::get('/', [AcceuilController::class, 'index'])->name('acceuil');
 Route::get('/evenements', [EvenementController::class, 'index'])->name('public.evenements');
+
+// Private routes
 Route::get('/admin-tableau-de-bord', [AdminTableaudebordController::class, 'adminTableaudebord'])->name('private.admintableaudebord');
 Route::get('/promoteur-tableau-de-bord', [PromoteurTableaudebordController::class, 'promoteurTableaudebord'])->name('private.promoteurtableaudebord');
 Route::get('/abonne-tableau-de-bord', [AbonneTableaudebordController::class, 'abonneTableaudebord'])->name('private.abonnetableaudebord');
 
+// Auth routes
+Route::get('/inscription-option', [AuthController::class, 'incriptionOption'])->name('public.inscription-option');
 
